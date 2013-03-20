37288ebae126fbef16f2f5bd0943a8d82badbdfe
Joaquín Sorianello <joac@gcoop.coop>
2013-03-20 19:35:45 -0300

    Termino boceto de aplicacion flask

--- app/app.py
+++ app/app.py
@@ -12,19 +12,46 @@ Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active_page='Inicio')
 
 @app.route('/libros')
 def libros():
-    return render_template('catalogo.html', active_page='Libros')
+    datos = [
+        ('Historia de Cronopios y de Famas', 'Julio Cortazar', 'pdf/sample.pdf', 'libros/cronopios_famas.png'),
+        ('Rayuela', 'Julio Cortazar', 'pdf/sample.pdf', 'libros/rayuela.png'),
+        ('El Aleph', 'Jorge Luis Borges', 'pdf/sample.pdf', 'libros/elaleph.png'),
+    ]
+
+    datos = datos * 50
+
+    return render_template('catalogo.html', active_page='Libros', datos=datos)
 
 @app.route('/cuentos')
 def cuentos():
-    return render_template('cuentos.html')
+    datos = [
+        ('Historia de Cronopios y de Famas', 'Julio Cortazar', 'pdf/sample.pdf', 'libros/cronopios_famas.png'),
+        ('Rayuela', 'Julio Cortazar', 'pdf/sample.pdf', 'libros/rayuela.png'),
+        ('El Aleph', 'Jorge Luis Borges', 'pdf/sample.pdf', 'libros/elaleph.png'),
+    ]
+
+    datos = datos * 50
+
+    return render_template('catalogo.html', active_page='Libros', datos=datos)
+
+
+
 
 @app.route('/revistas')
 def revistas():
-    return render_template('revistas.html')
+    datos = [
+        (u'El Pendulo N°1', u'Varios', u'pdf/sample.pdf', u'revistas/pendulo1.png'),
+        (u'El Pendulo N°2', u'Varios', u'pdf/sample.pdf', u'revistas/pendulo2.png'),
+        (u'El Pendulo N°5', u'Varios', u'pdf/sample.pdf', u'revistas/pendulo5.png'),
+    ]
+
+    datos = datos * 50
+
+    return render_template('catalogo.html', active_page='Revistas', datos=datos)
 
 @app.route('/acerca_de')
 def acerca_de():
