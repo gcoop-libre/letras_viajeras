168578aefba317628383d3cf80b5a1d27473aa70
Joaquín Sorianello <joac@gcoop.coop>
2013-03-22 12:08:51 -0300

    configs del ap, por ahora

---
+++ instalar_paquetes.sh
@@ -0,0 +1,8 @@
+opkg update
+opkg install kmod-usb-storage
+opkg install mini-httpd
+opkg install block-mount
+opkg install kmod-fs-vfat
+opkg install kmod-nls-base
+opkg install kmod-nls-cp437
+opkg install kmod-nls-iso8859-1
